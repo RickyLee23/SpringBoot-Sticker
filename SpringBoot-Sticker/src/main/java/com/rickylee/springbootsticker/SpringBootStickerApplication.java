b1296ec322cd47d1ef65c0bef001c82a7dc1ccca
Ricky <ricky101305324@gmail.com>
2022-09-05 19:26:51 +0800

    feat: initial Spring Boot project

---
+++ SpringBoot-Sticker/src/main/java/com/rickylee/springbootsticker/SpringBootStickerApplication.java
@@ -0,0 +1,13 @@
+package com.rickylee.springbootsticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStickerApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Sticker/src/test/java/com/rickylee/springbootsticker/SpringBootStickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickylee.springbootsticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
